c17a90a61d63ff4b8ecb8f423b79f56b05d1cafa
Parth Solanki <100123534+paso-odoo@users.noreply.github.com>
2022-07-15 15:11:48 +0530

    Delete delivery_shiprocket directory

--- delivery_shiprocket/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import delivery_carrier
-from . import shiprocket_request
-from . import stock_picking
-from . import shiprocket_service
-from . import stock_package_type
